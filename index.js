4faa9b16a6ca0cb69924cc19c883d3cccc545394
Mina Hosseini <minaa.hosseinii@gmail.com>
2021-03-11 18:23:51 +0100

    basic html and css for question card added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('.form');
 const input = document.querySelector('.form__input');
-const instructionBox = document.querySelector('.instruction');
+// const instructionBox = document.querySelector('.instruction');
 
 
 let continent = input.value;
@@ -21,10 +21,16 @@ form.addEventListener('submit', event => {
       return;
     } else {
       console.log(data);
-      form.style.visibility = 'hidden';
-      instructionBox.style.visibility = 'visible';
+      // form.style.visibility = 'hidden';
+      // instructionBox.style.visibility = 'visible';
     }
   }
+  // 0: {number: 1, question: "https://restcountries.eu/data/cpv.svg", answer: "Praia", options: Array(4)}
+  // 1: {number: 2, question: "https://restcountries.eu/data/caf.svg", answer: "Bangui", options: Array(4)}
+  // 2: {number: 3, question: "https://restcountries.eu/data/tcd.svg", answer: "N'Djamena", options: Array(4)}
+  // 3: {number: 4, question: "https://restcountries.eu/data/com.svg", answer: "Moroni", options: Array(4)}
+  // 4: {number: 5, question: "https://restcountries.eu/data/cog.svg", answer: "Brazzaville", options: Array(4)}
+ 
 
   getapi(`http://localhost:8080/api/?continent=${continent}`);
 
